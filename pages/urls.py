ddf5bd2aa2c156a80e35ae1517db8bb296d5363e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-22 14:44:10 -0300

    added login page functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from pages.views import HomePageView
+from pages.views import HomePageView, LoginPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('login/', LoginPageView.as_view(), name='login'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class LoginPageView(TemplateView):
+    template_name = 'pages/login.html'
