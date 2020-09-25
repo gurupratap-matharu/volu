d0f8aa47a8c6541d10d66bb49157653aeaa28f93
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-25 10:55:25 -0300

    shortened the contact and feedback forms

--- pages/forms.py
+++ pages/forms.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 class ContactForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Message', max_length=600, widget=forms.Textarea)
+    message = forms.CharField(label='Message', max_length=600, widget=forms.Textarea(attrs={'cols': 80, 'rows': 5}))
 
     def send_mail(self):
         logger.info('Sending email...')
@@ -30,7 +30,7 @@ class ContactForm(forms.Form):
 class FeedbackForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Feedback', max_length=400, widget=forms.Textarea)
+    message = forms.CharField(label='Feedback', max_length=400, widget=forms.Textarea(attrs={'cols': 80, 'rows': 5}))
 
     def send_mail(self):
         logger.info('Sending feedback...')
