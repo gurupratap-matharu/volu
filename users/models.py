b3ebc7ae5f142c09574c458df59a3a8dc898df73
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-27 14:11:14 -0300

    created the profile model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,31 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bio = models.CharField(max_length=600, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    country = CountryField(blank_label='(select country)')
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile')
+
+
+@receiver(post_save, sender=get_user_model())
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=get_user_model())
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
