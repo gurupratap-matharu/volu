2ed314393f60207580f0909d926ee05754d69b52
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-29 09:19:16 -0300

    minor correction in signals

--- users/models.py
+++ users/models.py
@@ -30,12 +30,12 @@ class Profile(models.Model):
         return user.is_superuser or self.user == user
 
 
-@ receiver(post_save, sender=get_user_model())
+@receiver(post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@ receiver(post_save, sender=get_user_model())
+@receiver(post_save, sender=get_user_model())
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
