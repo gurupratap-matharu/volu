4c69aa4f30841b83ed7a3a1a9a581d7473699ce0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-29 09:34:33 -0300

    generating profile data in user factory

--- users/factories.py
+++ users/factories.py
@@ -1,8 +1,11 @@
 import factory
 from django.contrib.auth import get_user_model
+from faker import Faker
+
+fake = Faker()
 
 
-class UserFactory(factory.django.DjangoModelFactory):
+class AbstractUserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
 
@@ -10,3 +13,14 @@ class UserFactory(factory.django.DjangoModelFactory):
     last_name = factory.Faker('last_name')
     email = factory.Faker('email')
     username = factory.Faker('user_name')
+
+
+def user_factory():
+    user = AbstractUserFactory()
+
+    user.profile.country = fake.country_code()
+    user.profile.bio = fake.text()
+    user.profile.birth_date = fake.date()
+
+    user.save()
+    return user
