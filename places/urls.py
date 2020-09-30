d9a9e6c46f7b6145f15bd1fdc1b93f39297e85ee
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-09-30 16:06:34 -0300

    CRUD operations on place model

--- places/urls.py
+++ places/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from places.views import PlaceDetailView, PlaceListView
+from places.views import (PlaceCreate, PlaceDelete, PlaceDetailView,
+                          PlaceListView, PlaceUpdate)
 
 urlpatterns = [
     path('', PlaceListView.as_view(), name='place_list'),
     path('<uuid:pk>/', PlaceDetailView.as_view(), name='place_detail'),
+    path('create/', PlaceCreate.as_view(), name='place_create'),
+    path('<uuid:pk>/update/', PlaceUpdate.as_view(), name='place_update'),
+    path('<uuid:pk>/delete/', PlaceDelete.as_view(), name='place_delete'),
 ]
--- places/views.py
+++ places/views.py
@@ -1,6 +1,16 @@
-from django.views.generic import DetailView, ListView
+import logging
+
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
 from places.models import Place
+
+logger = logging.getLogger(__name__)
 
 
 class PlaceListView(ListView):
@@ -14,3 +24,43 @@ class PlaceDetailView(DetailView):
     model = Place
     context_object_name = 'place'
     template_name = 'places/place_detail.html'
+
+
+class PlaceCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Place
+    fields = ['name', 'description', 'email', 'address1', 'address2', 'zip_code', 'city', 'country', 'is_active']
+    sucess_message = "%(name)s successfully created!"
+
+    def form_valid(self, form):
+        form.instance.host = self.request.user
+        return super().form_valid(form)
+
+
+class PlaceUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Place
+    fields = ['name', 'description', 'email', 'address1', 'address2', 'zip_code', 'is_active']
+    sucess_message = "%(name)s successfully updated!"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        if not obj.can_update(self.request.user):
+            logger.warning('Possible attack: \nuser: %s\nobj: %s', self.request.user, obj)
+            raise Http404
+        return obj
+
+
+class PlaceDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Place
+    success_url = reverse_lazy('home')
+    success_message = "Place deleted successfully!"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset=queryset)
+        if not obj.can_delete(self.request.user):
+            logger.warning('Possible attack: \nuser: %s\nobj: %s', self.request.user, obj)
+            raise Http404
+        return obj
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(PlaceDelete, self).delete(request, *args, **kwargs)
