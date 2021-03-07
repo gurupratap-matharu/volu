596c496f12c5afb3468fe58e188b3bacb0bd0311
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-07 20:42:29 -0300

    renamed email fields

--- places/factories.py
+++ places/factories.py
@@ -3,17 +3,20 @@ from users.factories import AbstractUserFactory
 
 from places.models import Place
 
+NAMES = ['Hostel', 'Beautiful House', 'Wineyard', 'Organic Farm', 'Country Cabin']
+
 
 class PlaceFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Place
 
-    name = factory.Faker('catch_phrase')
+    name = factory.Faker('random_element', elements=NAMES)
     description = factory.Faker('text')
-    email = factory.Faker('company_email')
+    email = factory.Faker('email')
     address1 = factory.Faker('address')
     address2 = factory.Faker('address')
     city = factory.Faker('city')
     zip_code = factory.Faker('zipcode')
     country = factory.Faker('country_code')
     host = factory.SubFactory(AbstractUserFactory)
+
+    class Meta:
+        model = Place
