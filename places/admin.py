6d15ed131cda4951e065da3dafb18e18ad4ba8a0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-07 20:57:51 -0300

    added places to django admin interface

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from places.models import Place
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'city', 'country', 'is_active', 'ratings', 'host',)
+    list_filter = ('is_active', 'created_on',)
+    search_fields = ('name', 'email',)
+    list_editable = ('is_active',)
+    readonly_fields = ('ratings',)
+    raw_id_fields = ('host',)
+    date_hierarchy = 'created_on'
+    ordering = ('is_active', 'country', 'city',)
+
+
+admin.site.register(Place, PlaceAdmin)
