9a62e15cc31e7405b9a62ea04749cd77a0207f27
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-07 22:58:51 -0300

    added ptsvd to connect remote debugger

--- manage.py
+++ manage.py
@@ -15,6 +15,20 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if (os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN")) and os.environ.get(
+        "VSCODE_DEBUGGER", False
+    ):
+        import ptvsd  # noqa
+
+        ptvsd_port = 4000
+
+        try:
+            ptvsd.enable_attach(address=("0.0.0.0", ptvsd_port), redirect_output=True)
+            print("Started ptvsd at port %s." % ptvsd_port)
+        except OSError:
+            print("ptvsd port %s already in use." % ptvsd_port)
+
     execute_from_command_line(sys.argv)
 
 
