84084ac0fb943d6c332e331bd15225d401a851b4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-21 15:25:45 -0300

    added app_name parameter as it is defined in the main urlconf

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from users.views import ProfileDetailView, ProfileUpdate
 
+app_name = 'users'
+
 urlpatterns = [
     path('<uuid:pk>/', ProfileDetailView.as_view(), name='profile_detail'),
     path('<uuid:pk>/update/', ProfileUpdate.as_view(), name='profile_update'),
