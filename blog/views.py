e45d90986337ae31d023c09a08a27d228c4137d0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-21 13:45:52 -0300

    added post list view as a generic list view to list all blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'post_list'
+    queryset = Post.published.all()
