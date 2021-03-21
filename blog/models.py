c8f4f6345772d1e5ec94287731cd32c6e07bf142
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-21 12:48:30 -0300

    initial commit for blog app

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='blog_posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
