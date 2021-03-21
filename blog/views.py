621851303491c031a6087182f243c0ff0b63c623
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-21 15:19:08 -0300

    over ridden get_object() method on detailview for custom object fetch

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
 from .models import Post
@@ -5,6 +6,20 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'
     context_object_name = 'post_list'
     queryset = Post.published.all()
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+    def get_object(self, queryset):
+        post = get_object_or_404(Post, slug=self.kwargs['slug'],
+                                 status='published',
+                                 publish__year=self.kwargs['year'],
+                                 publish__month=self.kwargs['month'],
+                                 publish__day=self.kwargs['day'])
+        print('veer getting object...')
+        print('veer post: {}'.format(post))
+        return post
