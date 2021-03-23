c604e835011e786fe5c2641593035869442a3fb8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-23 10:54:26 -0300

    added form_valid method to post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import FormMixin
 
+from .forms import PostShareForm
 from .models import Post
 
 
@@ -11,9 +13,20 @@ class PostListView(ListView):
     paginate_by = 12
 
 
-class PostDetailView(DetailView):
+class PostDetailView(FormMixin, DetailView):
     model = Post
     context_object_name = 'post'
+    form_class = PostShareForm
+    success_message = "Post shared successfully!"
+
+    def get_success_url(self) -> str:
+        return self.get_object().get_absolute_url()
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
 
     def get_object(self, queryset=None):
         post = get_object_or_404(Post, slug=self.kwargs['post'],
@@ -21,6 +34,4 @@ class PostDetailView(DetailView):
                                  publish__year=self.kwargs['year'],
                                  publish__month=self.kwargs['month'],
                                  publish__day=self.kwargs['day'])
-        print('veer getting object...')
-        print('veer post: {}'.format(post))
         return post
