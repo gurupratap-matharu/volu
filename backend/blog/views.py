ce089ee7fe5f07becd6d61ca6209d27728a5006f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-24 14:35:34 -0300

    added post share view

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, ListView
-from django.views.generic.edit import FormMixin
+from django.views.generic import DetailView, FormView, ListView
+from django.views.generic.detail import SingleObjectMixin
 
 from .forms import PostShareForm
 from .models import Post
@@ -13,20 +13,10 @@ class PostListView(ListView):
     paginate_by = 12
 
 
-class PostDetailView(FormMixin, DetailView):
+class PostDetailView(DetailView):
     model = Post
     context_object_name = 'post'
-    form_class = PostShareForm
-    success_message = "Post shared successfully!"
-
-    def get_success_url(self) -> str:
-        return self.get_object().get_absolute_url()
-
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        return self.form_invalid(form)
+    extra_context = {'form': PostShareForm()}
 
     def get_object(self, queryset=None):
         post = get_object_or_404(Post, slug=self.kwargs['post'],
@@ -35,3 +25,24 @@ class PostDetailView(FormMixin, DetailView):
                                  publish__month=self.kwargs['month'],
                                  publish__day=self.kwargs['day'])
         return post
+
+
+class PostShare(SingleObjectMixin, FormView):
+    form_class = PostShareForm
+
+    def form_valid(self, form):
+        post = self.get_object()
+        post.url = self.request.build_absolute_uri(post.get_absolute_url())
+        form.send_mail(post)
+        return super().form_valid(form)
+
+    def get_object(self, queryset=None):
+        post = get_object_or_404(Post, slug=self.kwargs['post'],
+                                 status='published',
+                                 publish__year=self.kwargs['year'],
+                                 publish__month=self.kwargs['month'],
+                                 publish__day=self.kwargs['day'])
+        return post
+
+    def get_success_url(self) -> str:
+        return self.get_object().get_absolute_url()
