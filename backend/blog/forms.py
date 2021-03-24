c7a4c0779de94a684cf34b674aa8da8c3ff4a336
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-24 14:36:06 -0300

    added send_mail method to postshare form

--- backend/blog/forms.py
+++ backend/blog/forms.py
@@ -1,5 +1,9 @@
+import logging
+
 from django import forms
-from django.forms import widgets
+from django.core.mail import send_mail
+
+logger = logging.getLogger(__name__)
 
 
 class PostShareForm(forms.Form):
@@ -7,3 +11,14 @@ class PostShareForm(forms.Form):
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+    def send_mail(self, post=None):
+        logger.info('sending post share email...')
+
+        subject = '{0} recommends you read {1}'.format(self.cleaned_data['name'], post.title)
+        message = "Read {} at {}\n\n{}\'s comments: {}".format(post.title, post.url,
+                                                               self.cleaned_data['name'],
+                                                               self.cleaned_data['comments'])
+        send_mail(subject=subject, message=message,
+                  from_email='admin@site.domain',
+                  recipient_list=[self.cleaned_data['to']], fail_silently=False)
