5b047f61bf0b8c12231be11d24f058b6917f610b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-25 17:55:25 -0300

    added endpoint to show similar posts based on tag

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/', PostListView.as_view(), name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', PostDetailView.as_view(), name='post_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/share/', PostShare.as_view(), name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/comment/', CommentCreate.as_view(), name='comment_create'),
