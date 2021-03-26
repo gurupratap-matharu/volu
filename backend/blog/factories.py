12458a2831d94d598ad5b54de928a88962154b0e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-26 13:46:51 -0300

    improved the post factory with custom dates

--- backend/blog/factories.py
+++ backend/blog/factories.py
@@ -1,4 +1,5 @@
 import factory
+from django.utils import timezone
 from users.factories import UserFactory
 
 from .models import Post
@@ -6,9 +7,10 @@ from .models import Post
 
 class PostFactory(factory.django.DjangoModelFactory):
     title = factory.Faker('sentence')
-    slug = factory.Faker('slug', value=title)
+    slug = factory.LazyAttribute(lambda obj: factory.Faker('slug', value=obj.title).generate())
     body = factory.Faker('paragraph', nb_sentences=50)
     author = factory.SubFactory(UserFactory)
+    publish = factory.Faker('date_time_this_century', tzinfo=timezone.get_current_timezone())
     status = factory.Faker('random_element', elements=('draft', 'published'))
 
     class Meta:
