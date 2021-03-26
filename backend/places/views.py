3a627d4e08d92d197842b999d60c74ac700ddf5b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-26 17:30:57 -0300

    added custom get_object() method

--- backend/places/views.py
+++ backend/places/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
@@ -11,6 +12,14 @@ from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
 from places.models import Place
 
 logger = logging.getLogger(__name__)
+
+
+def get_place_object(obj):
+    return get_object_or_404(Place, slug=obj.kwargs['place'],
+                             is_active=True,
+                             created_on__year=obj.kwargs['year'],
+                             created_on__month=obj.kwargs['month'],
+                             created_on__day=obj.kwargs['day'])
 
 
 class PlaceListView(ListView):
@@ -24,6 +33,9 @@ class PlaceDetailView(DetailView):
     model = Place
     context_object_name = 'place'
     template_name = 'places/place_detail.html'
+
+    def get_object(self, queryset=None):
+        return get_place_object(self)
 
 
 class PlaceCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
@@ -43,7 +55,7 @@ class PlaceUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     template_name = 'places/place_update_form.html'
 
     def get_object(self, queryset=None):
-        obj = super().get_object()
+        obj = get_place_object(self)
         if not obj.can_update(self.request.user):
             logger.warning('Possible attack: \nuser: %s\nobj: %s', self.request.user, obj)
             raise Http404
@@ -56,7 +68,7 @@ class PlaceDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     success_message = "Place deleted successfully!"
 
     def get_object(self, queryset=None):
-        obj = super().get_object(queryset=queryset)
+        obj = get_place_object(self)
         if not obj.can_delete(self.request.user):
             logger.warning('Possible attack: \nuser: %s\nobj: %s', self.request.user, obj)
             raise Http404
