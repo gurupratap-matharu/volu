004e7de4ce4094704bc8ec89d511ec0acb613e09
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-26 15:57:58 -0300

    prepopulated slug field for place model

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -3,15 +3,14 @@ from django.contrib import admin
 from places.models import Place
 
 
+@admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'city', 'country', 'is_active', 'ratings', 'host',)
+    list_display = ('name', 'slug', 'email', 'city', 'country', 'is_active', 'ratings', 'host',)
     list_filter = ('is_active', 'created_on',)
     search_fields = ('name', 'email',)
     list_editable = ('is_active',)
+    prepopulated_fields = {'slug': ('name',)}
     readonly_fields = ('ratings',)
     raw_id_fields = ('host',)
     date_hierarchy = 'created_on'
     ordering = ('is_active', 'country', 'city',)
-
-
-admin.site.register(Place, PlaceAdmin)
