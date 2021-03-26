0414f4d837fded751dbc9f67a162d1e9e81de808
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-03-26 12:49:29 -0300

    added Profile model to django admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,14 +4,25 @@ from django.contrib.auth.admin import UserAdmin
 
 from users.forms import CustomUserChangeForm, CustomUserCreationForm
 
+from .models import Profile
+
 CustomUser = get_user_model()
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', ]
+    list_display = ('email', 'username', 'is_staff',)
+    search_fields = ('email', 'username',)
+    ordering = ('email',)
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birth_date', 'country',)
+    list_filter = ('country',)
+    search_fields = ('country',)
+    raw_id_fields = ('user',)
+    ordering = ('country',)
