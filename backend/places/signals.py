30f783b458abbb211459c90883c046ab5c0e9293
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-28 19:28:29 +0530

    Add signal with pre-save hook for thumbnail generation

---
+++ backend/places/signals.py
@@ -0,0 +1,30 @@
+
+import logging
+from io import BytesIO
+
+from django.core.files.base import ContentFile
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from PIL import Image
+
+from places.models import PlaceImage
+
+THUMBNAIL_SIZE = (300, 300)
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=PlaceImage)
+def generate_thumbnail(sender, instance, **kwargs):
+    logger.info("Generating thumbnail for place %d", str(instance))
+
+    image = Image.open(instance.image)
+    image = image.convert("RGB")
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+    temp_thumb = BytesIO()
+    image.save(temp_thumb, "JPEG")
+    temp_thumb.seek(0)
+
+    # set save=False, otherwise it will runn in an infinite loop
+    instance.thumbnail.save(instance.image.name, ContentFile(temp_thumb.read()), save=False)
+    temp_thumb.close()
