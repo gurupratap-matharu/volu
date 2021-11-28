99cf106b4ccc138a2e9c4fc7c536bebdb0639c1a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-28 21:16:55 +0530

    Improve naming convention for media

--- backend/places/utils.py
+++ backend/places/utils.py
@@ -1,8 +1,8 @@
 
 def place_image_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/place_<id>/<filename>
-    return 'place_{0}/images/{1}'.format(instance.place.id, filename)
+    return 'places/place_{0}/images/{1}'.format(instance.place.id, filename)
 
 
 def place_thumbnail_path(instance, filename):
-    return 'place_{0}/thumbnails/{1}'.format(instance.place.id, filename)
+    return 'places/place_{0}/thumbnails/{1}'.format(instance.place.id, filename)
