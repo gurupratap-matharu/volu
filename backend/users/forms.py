dd4ae503dc0f09086a6d5ae8e482066ded7085a3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-11-29 12:03:37 +0530

    Add profileupdate form with custom widget

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -1,5 +1,8 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from users.models import Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,3 +15,12 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username',)
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'birth_date', 'country',)
+        widgets = {
+            'birth_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class': 'datepicker', 'placeholder': 'Select a date', 'type': 'date'}),
+        }
