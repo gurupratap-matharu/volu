e083c4bb913e8b6d7872579d354fe75da5ea5154
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-01 20:37:43 +0530

    Add static CDN settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -172,6 +172,11 @@ if USE_SPACES:
     AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
+    # static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
     # public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
