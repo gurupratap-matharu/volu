e2e3419308c767fca35f63c905c3e8e514c58080
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-03 14:29:18 +0530

    Add unit tests for place detail view

--- backend/places/tests/test_views.py
+++ backend/places/tests/test_views.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from places.factories import PlaceFactory, PlaceImageFactory
+from places.views import PlaceDetailView
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+
+class PlaceListTests(TestCase):
+    pass
+
+
+class PlaceDetailTests(TestCase):
+    def setUp(self):
+        self.place = PlaceFactory()
+        self.images = PlaceImageFactory.create_batch(size=4, place=self.place)
+
+    def test_place_detail_resolves_place_detail_view(self):
+        view = resolve(self.place.get_absolute_url())
+        self.assertEqual(PlaceDetailView.as_view().__name__, view.func.__name__)
+
+    def test_place_detail_works(self):
+        response = self.client.get(self.place.get_absolute_url())
+        no_response = self.client.get('places/1234/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'places/place_detail.html')
+        self.assertContains(response, self.place.name)
+        self.assertContains(response, self.place.description)
+
+        urls = [x.image.url for x in self.images]
+        self.assertContains(response, urls)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertNotContains(response, 'Hi I should not be on this page!')
+
+
+class PlaceCreateTests(TestCase):
+    pass
+
+
+class PlaceUpdateTests(TestCase):
+    pass
+
+
+class PlaceDeleteTests(TestCase):
+    pass
