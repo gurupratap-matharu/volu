0fceb725cad47c1453577983a698c18bbdf79ac7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-03 14:29:00 +0530

    Add Place image to admin

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-
 from places.models import Place, PlaceImage
 
+from django.contrib import admin
+from django.utils.html import format_html
 
-class PlaceImageInline(admin.TabularInline):
+
+class PlaceImageInlineAdmin(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = ('thumbnail',)
 
 
 @admin.register(Place)
@@ -18,5 +20,23 @@ class PlaceAdmin(admin.ModelAdmin):
     raw_id_fields = ('host',)
     date_hierarchy = 'created_on'
     ordering = ('is_active', 'country', 'city',)
+    autocomplete_fields = ('tags',)
 
-    inlines = [PlaceImageInline, ]
+    inlines = [PlaceImageInlineAdmin, ]
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'place_name')
+    readonly_fields = ('thumbnail',)
+    search_fields = ('place__name',)
+    raw_id_fields = ('place',)
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="%s"/>' % obj.thumbnail.url)
+        return "-"
+    thumbnail_tag.short_description = "Thumbnail"
+
+    def place_name(self, obj):
+        return obj.place.name
