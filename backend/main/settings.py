631614cff0acfd305ca80ed253979c1e1587daf0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-04 18:19:17 +0530

    Add mailgun settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -233,7 +233,7 @@ if not DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.mailgun.org'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
 
