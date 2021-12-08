9421ef175ce62dd86c36474afee48b4fc56004f8
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-08 18:31:05 +0530

    Add payment success and cancel view

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+
+import stripe
+
+logger = logging.getLogger(__name__)
+
+
+class SubscriptionPageView(LoginRequiredMixin, TemplateView):
+    template_name = 'subscriptions/purchase.html'
+
+
+class PaymentSuccessView(LoginRequiredMixin, TemplateView):
+    template_name = 'subscriptions/payment_success.html'
+
+
+class PaymentCancelView(LoginRequiredMixin, TemplateView):
+    template_name = 'subscriptions/payment_cancel.html'
+
+
+@csrf_exempt
+def stripe_config(request):
+    if request.method == 'GET':
+        logger.info('sending stripe config...')
+        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+        return JsonResponse(stripe_config, safe=False)
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    if request.method == 'GET':
+        logger.info('creating stripe checkout session...')
+
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        client_reference_id = request.user.id if request.user.is_authenticated else None
+
+        domain_url = 'http://localhost/subscriptions/'
+        success_url = domain_url + 'success?session_id={CHECKOUT_SESSION_ID}'
+        cancel_url = domain_url + 'cancel/'
+        line_items = [{'price': settings.STRIPE_PRICE_ID, 'quantity': 1}]
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                client_reference_id=client_reference_id,
+                success_url=success_url,
+                cancel_url=cancel_url,
+                payment_method_types=['card'],
+                mode='subscription',
+                line_items=line_items
+            )
+
+            session_id = checkout_session['id']
+
+            return JsonResponse({'sessionId': session_id})
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
