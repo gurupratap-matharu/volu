284172f9973cc25120c1e6c94e081649cbbd4e6c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-08 18:32:23 +0530

    Add endpoint for purchasing subscription

---
+++ backend/subscriptions/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import (
+    PaymentCancelView, PaymentSuccessView, SubscriptionPageView, create_checkout_session,
+    stripe_config,
+)
+
+urlpatterns = [
+    path('', SubscriptionPageView.as_view(), name='subscriptions'),
+    path('config/', stripe_config, name='stripe-config'),
+    path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
+    path('success/', PaymentSuccessView.as_view(), name='payment-success'),
+    path('cancel/', PaymentCancelView.as_view(), name='payment-cancel')
+]
