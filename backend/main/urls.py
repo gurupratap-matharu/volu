4afe8a82b259c16ba06ab054a62ab165b5ef0ef3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-08 13:53:10 +0530

    Add rosetta endpoint to main urls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,38 +1,27 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-
+urlpatterns = i18n_patterns(
     # Django administration
     path('dj-admin/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
 
+    # Translation
+    path('rosetta/', include('rosetta.urls')),
+
     # Local apps
     path('', include('pages.urls', namespace='pages')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('places/', include('places.urls', namespace='places')),
     path('profile/', include('users.urls', namespace='users')),
+    path('subscriptions/', include('subscriptions.urls')),
+)
 
-]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
