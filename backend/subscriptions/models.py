749eade6849538ecf7a1603fa4f38a4e9914a909
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-08 18:30:02 +0530

    Create Stripe Customer

--- backend/subscriptions/models.py
+++ backend/subscriptions/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class StripeCustomer(models.Model):
+    user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=255)
+    stripe_subscription_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.email
