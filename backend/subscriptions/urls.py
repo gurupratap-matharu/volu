c6835b9f2738b80bbf9428eba0a4911c372fdc7a
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-08 20:23:37 +0530

    add web hook endpoint

--- backend/subscriptions/urls.py
+++ backend/subscriptions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     PaymentCancelView, PaymentSuccessView, SubscriptionPageView, create_checkout_session,
-    stripe_config,
+    stripe_config, stripe_webhook,
 )
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('config/', stripe_config, name='stripe-config'),
     path('create-checkout-session/', create_checkout_session, name='create-checkout-session'),
     path('success/', PaymentSuccessView.as_view(), name='payment-success'),
-    path('cancel/', PaymentCancelView.as_view(), name='payment-cancel')
+    path('cancel/', PaymentCancelView.as_view(), name='payment-cancel'),
+    path('webhook/', stripe_webhook, name='stripe-webhook'),
 ]
