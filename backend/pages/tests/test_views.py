8d0ed7fcb8a4a29a6df6ea4d858a29b3f8e33e1f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-11 20:00:03 +0530

    Basic unit test for search page view

--- backend/pages/tests/test_views.py
+++ backend/pages/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from pages.views import AboutPageView, HomePageView
+from pages.views import AboutPageView, HomePageView, SearchPageView
 
 
 class HomePageTests(TestCase):
@@ -32,3 +32,19 @@ class AboutPageTests(TestCase):
     def test_about_page_resolves_homepageview(self):
         view = resolve(reverse('pages:about'))
         self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
+
+
+class SearchPageTests(TestCase):
+    def test_search_page_works(self):
+        response = self.client.get(reverse('pages:search'))
+        no_response = self.client.get('search-results')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Search')
+        self.assertTemplateUsed(response, 'pages/search.html')
+        self.assertNotContains(response, 'Hi I should not be on this page.')
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_search_page_resolves_searchpageview(self):
+        view = resolve(reverse('pages:search'))
+        self.assertEqual(view.func.__name__, SearchPageView.as_view().__name__)
