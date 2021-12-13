000d05ba098d6bf90bfa782c0a41da3ccfa92d54
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-13 12:54:18 +0530

    formatting with autopep8

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+
 from taggit.managers import TaggableManager
 
 
@@ -19,27 +20,26 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
-    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='blog_posts')
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL, null=True, related_name='blog_posts')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    class Meta:
-        ordering = ('-publish',)
-
     objects = models.Manager()
     published = PublishedManager()
     tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year,
-                                                 self.publish.month,
-                                                 self.publish.day,
-                                                 self.slug])
+        return reverse('blog:post_detail', args=[
+            self.publish.year, self.publish.month, self.publish.day, self.slug])
 
     def get_share_url(self):
         return self.get_absolute_url() + 'share/'
