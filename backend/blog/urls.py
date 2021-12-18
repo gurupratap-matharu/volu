f5ed548cc67b12ea93191fe59bae029ff786122c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-12-18 21:27:58 +0530

    Add rss feeds for blog posts

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .feeds import LatestPostsFeed
 from .views import CommentCreate, PostDetailView, PostListView, PostShare
 
 app_name = 'blog'
@@ -10,4 +11,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', PostDetailView.as_view(), name='post_detail'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/share/', PostShare.as_view(), name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/comment/', CommentCreate.as_view(), name='comment_create'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
