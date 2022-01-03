45d5571089714838670b8598864bbd5ba885362f
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-01-03 22:29:05 +0530

    Add subject field to contact form

--- backend/pages/forms.py
+++ backend/pages/forms.py
@@ -17,22 +17,19 @@ class SearchForm(forms.Form):
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Message', max_length=600, widget=forms.Textarea(attrs={'cols': 80, 'rows': 5}))
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(max_length=600, widget=forms.Textarea(attrs={'cols': 80, 'rows': 5}))
+
+    def clean_subject(self):
+        subject = ''.join(self.cleaned_data['subject'].splitlines())
+        return '[Contact form] ' + subject
 
     def send_mail(self):
 
-        subject = 'Volu Contact message from {}'.format(self.cleaned_data['name'])
-        message = 'From: {0}\nEmail: {1}\n\n{2}'.format(
-            self.cleaned_data['name'],
-            self.cleaned_data['email'],
-            self.cleaned_data['message']
-        )
-
-        logger.info('subject: %s', subject)
-        logger.info('message: %s', message)
-        logger.info('Sending contact email...')
+        subject = self.cleaned_data['subject']
+        message = 'From: {name}\nEmail: {email}\n\n{message}'.format(**self.cleaned_data)
 
         send_mail(subject=subject,
                   message=message,
