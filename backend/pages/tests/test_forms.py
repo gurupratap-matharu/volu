d5c9edd1bdf4b259100e71e90204fc1b1440b218
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-01-03 22:40:28 +0530

    Add unit test for empty name and invalid email fields

--- backend/pages/tests/test_forms.py
+++ backend/pages/tests/test_forms.py
@@ -5,10 +5,14 @@ from pages.forms import ContactForm, FeedbackForm
 
 
 class TestContactForm(TestCase):
+    def setUp(self) -> None:
+        self.url = '/contact/'
+
     def test_valid_contact_form_sends_email(self):
         form = ContactForm({
             'name': 'Veer',
             'email': 'veerplaying@gmail.com',
+            'subject': 'Food for thought',
             'message': 'In the darkest of times you will see the brightest of stars',
         })
         self.assertTrue(form.is_valid())
@@ -17,12 +21,31 @@ class TestContactForm(TestCase):
             form.send_mail()
 
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Volu Contact message from Veer')
+        self.assertEqual(mail.outbox[0].subject, '[Contact form] Food for thought')
         self.assertGreaterEqual(len(cm.output), 1)
 
     def test_invalid_contact_form(self):
         form = ContactForm({'message': 'Hi there'})
         self.assertFalse(form.is_valid())
+
+    def test_invalid_email(self):
+        response = self.client.post(self.url, {
+            'name': 'A Random Hacker',
+            'email': 'xxx',
+            'subject': 'Hello',
+            'message': 'Hello, World!',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'email', ['Enter a valid email address.'])
+
+    def test_empty_name(self):
+        response = self.client.post(self.url, {
+            'name': '',
+            'email': 'a.random@example.com',
+            'subject': 'Hello',
+            'message': 'Hello, World!',
+        })
+        self.assertFormError(response, 'form', 'name', ['This field is required.'])
 
 
 class FeedbackFormTest(TestCase):
